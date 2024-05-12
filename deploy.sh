74ebb8045ecdfcb568b9c2d04862ec1735d8334d
jayanth <83051900+jayan@users.noreply.github.com>
2024-05-12 19:27:56 +0530

    Update

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ if [[ "$1" == "devchanged" ]]; then
     docker tag "react:${IMAGE_COUNT}" "${DOCKER_USERNAME}/dev:Latest${IMAGE_COUNT}"
     docker push "${DOCKER_USERNAME}/dev:Latest${IMAGE_COUNT}"
     export IMAGE_NAME="react:${IMAGE_COUNT}"  # Export before running docker-compose
-    docker compose-up 
+    docker compose up d
 elif [[ "$1" == "devmergedmaster" ]]; then
     echo "Tagging and pushing image to prod repository..."
     IMAGE_COUNT="$2"  # Use the count passed as an argument
@@ -19,7 +19,7 @@ elif [[ "$1" == "devmergedmaster" ]]; then
     docker push "${DOCKER_USERNAME}/prod:Latest${IMAGE_COUNT}"
     docker push "${DOCKER_USERNAME}/dev:Latest${IMAGE_COUNT}"
     export IMAGE_NAME="react:${IMAGE_COUNT}"  # Export before running docker-compose
-    docker compose-up 
+    docker compose up -d
 else
     echo "Invalid argument. Please provide either 'devchanged' or 'devmergedmaster'."
     exit 1
